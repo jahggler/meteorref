132221a007eef38ad5354bf4da33a74ff89ee7a1
Alex Kuzmenok <kmz111@gmail.com>
2016-01-15 09:59:35 +0100

    created package-testing npm package dependency example

---
+++ package-testing/npm.js
@@ -0,0 +1,12 @@
+// This example show different possibilities of a test
+
+// package.js can have different NPM dependencies and use NPM modules
+Npm.depends({
+    'fs-extra': '0.16.5'
+});
+
+// then in a test file called npm.js (for server test of course)
+Tinytest.add('NPM modules - checking availability of npm', function (test) {
+  var fs = Npm.require('fs');
+  test.equal(typeof fs.readFile, 'function', 'Expected readFile to be available in NPM package');
+});
